676cd057d737eb6a1b5a647efece8c99a3d4e633
Piter Garcia <pzg8794@rit.edu>
2025-10-31 06:07:51 -0400

    Add deployment scripts

--- dynamic_exp_runner.sh
+++ dynamic_exp_runner.sh
@@ -31,7 +31,7 @@ export PYTHONPATH="$(pwd):$PYTHONPATH"
 cd Dynamic_Routing_Eval_Framework
 
 
-python3 << PYEOF # FIX: Use python3 for compatibility with GCP's Ubuntu 22.04 default
+python3 << 'PYEOF' # FIX: Use python3 for compatibility with GCP's Ubuntu 22.04 default
 import sys
 
 

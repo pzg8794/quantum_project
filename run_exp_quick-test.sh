bfcccf00e70ceb9467a22f218b37743f5e113250
Piter Garcia <pzg8794@rit.edu>
2025-10-31 05:26:45 -0400

    Add deployment scripts

---
+++ run_exp_quick-test.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set +e
+./1_startup.sh
+./dynamic_exp_runner.sh 100 1 100 12345 "quick-test" 0.25 None
+./3_push_results.sh
+gcloud compute instances add-metadata $(hostname) --zone=us-central1-a --metadata=status=complete
+echo "Test DONE"

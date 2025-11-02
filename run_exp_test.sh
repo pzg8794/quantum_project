71f4288b26210857c58507348d2758e208b633e9
Piter Garcia <pzg8794@rit.edu>
2025-11-02 02:15:11 -0500

    Add deployment scripts and results

--- run_exp_test.sh
+++ run_exp_test.sh
@@ -6,5 +6,4 @@ ALLOCATOR=$2
 
 ./dynamic_exp_runner.sh 100 1 100 12345 "$MODE" 0.25 "$ALLOCATOR"
 ./3_push_results.sh
-gcloud compute instances add-metadata $(hostname) --zone=us-central1-a --metadata=status=complete
 echo "Test DONE"

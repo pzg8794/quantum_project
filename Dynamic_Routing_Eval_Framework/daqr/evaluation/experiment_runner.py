e4422aeec658b08477bd8c90ac139e4afe77f195
Piter Garcia <pzg8794@rit.edu>
2025-11-09 18:26:23 -0500

    backup gcp-main

--- Dynamic_Routing_Eval_Framework/daqr/evaluation/experiment_runner.py
+++ Dynamic_Routing_Eval_Framework/daqr/evaluation/experiment_runner.py
@@ -1,4 +1,4 @@
-from    daqr.config.experiment_config import ExperimentConfiguration
+from    daqr.config.experiment_config import ExperimentConfiguration, QuantumAlgorithmLock
 from    tqdm    import tqdm
 import  numpy as np, copy
 import  gc, time
@@ -26,6 +26,7 @@ class QuantumExperimentRunner:
         self.configs.base_seed = base_seed
         
         self.use_locks = use_locks
+        self.resource_lock = QuantumAlgorithmLock(cooldown_seconds=1)
         
         self.id = id
         self.results = {}
